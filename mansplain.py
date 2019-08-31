8c9e809d6995d52a536ed05e10c07d66e48787d2
Evan <evanmdoyle@gmail.com>
2019-08-30 17:24:52 -0700

    Python script created, TODO: stream the output

---
+++ mansplain.py
@@ -0,0 +1,26 @@
+import sys
+import time
+from subprocess import Popen, PIPE
+
+
+def _slow_read(output) -> None:
+    print("Well basically,")
+    print()
+
+    for line in output:
+        for c in line:
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(0.01)
+
+
+def mansplain(command: str) -> None:
+    man_pipe = Popen(["man", command], stdout=PIPE)
+    _slow_read(man_pipe.communicate()[0].decode("utf-8"))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Well basically, this script takes 1 argument from the command line.")
+
+    mansplain(sys.argv[1])
