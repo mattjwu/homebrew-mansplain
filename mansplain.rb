22679f243c1c32d80dfe1e257a459addd69e7e46
Evan <evanmdoyle@gmail.com>
2019-08-30 17:55:53 -0700

    Add first attempt at a Formula

---
+++ mansplain.rb
@@ -0,0 +1,12 @@
+class Mansplain < Formula
+  desc "Basically it explains stuff to you"
+  homepage ""
+  url "https://github.com/emdoyle/homebrew-mansplain/raw/master/mansplain.tar.gz"
+  sha256 "d88c1c67e1a097c0ff1bf5f645e78cdc1af060236efdd5d48071a785941e33c7"
+
+  depends_on "python"
+
+  def install
+    virtualenv_install_with_resources
+  end
+end
