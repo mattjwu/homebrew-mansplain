9b7562e73fb2c05e1367a61da33ba6a43b1efecf
Evan <evanmdoyle@gmail.com>
2019-08-30 17:57:22 -0700

    Version number added

--- mansplain.rb
+++ mansplain.rb
@@ -3,6 +3,7 @@ class Mansplain < Formula
   homepage ""
   url "https://github.com/emdoyle/homebrew-mansplain/raw/master/mansplain.tar.gz"
   sha256 "d88c1c67e1a097c0ff1bf5f645e78cdc1af060236efdd5d48071a785941e33c7"
+  version "1"
 
   depends_on "python"
 
